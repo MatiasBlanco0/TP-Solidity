27c7a170f3979587deb9b933659ed1ff9cff1299
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-06-10 08:44:17 -0300

    Update EstudianteAlt.sol

--- EstudianteAlt.sol
+++ EstudianteAlt.sol
@@ -10,6 +10,11 @@ contract Estudiante{
     mapping (string => uint8) private _notas_materias;
     //Array de strings que guarda todas las materias seteadas
     string[] private _materias;
+    //Mapping de permisos
+    mapping (address => bool) _permisos;
+
+    //Evento de cuando se settea la nota
+    event SetNota(address docente, string materia, uint8 nota);
 
     /* Constructor del contrato
     Parametros: nombre (string), apellido (string) y curso (string)
@@ -37,12 +42,19 @@ contract Estudiante{
         return _curso;
     }
 
+    function set_permisos(address other_, bool estado_) public {
+        require(msg.sender == _docente, "Solo el docente del alumno puede setear permisos");
+
+        _permisos[other_] = estado_;
+    }
+
     /* Funcion que solo el docente que creo al Estudiante puede llamar.
     Parametros: nota (uint8) y materia (string)
     Si el valor de la nota es mayor a 100 o menor a 1, o la funcion es llamada por alguien que no es el docente, se para la funcion
-    Se pushea la materia a un array de materias y se actualiza el valor de la nota con la key materia*/
+    Se pushea la materia a un array de materias y se actualiza el valor de la nota con la key materia
+    Se hace emit al evento SetNota pasando msg.sender, materia_ y nota_*/
     function set_nota_materia(uint8 nota_, string memory materia_) public{
-        require(msg.sender == _docente, "Solo el docente del alumno puede modificar las notas");
+        require(msg.sender == _docente || _permisos[msg.sender] == true, "Solo personas con permisos pueden modificar las notas");
         require(nota_ <= 100, "La nota tiene que ser un numero entero del 1 al 100");
         require(nota_ >= 1, "La nota tiene que ser un numero entero del 1 al 100");
 
@@ -56,6 +68,8 @@ contract Estudiante{
             _materias.push(materia_);
         }
         _notas_materias[materia_] = nota_;
+
+        emit SetNota(msg.sender, materia_, nota_);
     }
 
     /* Funcion que devuelve la nota de la materia ingresada
